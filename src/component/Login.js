2b4afe1f09656411c51c4d61d325ceeaaa4a572d
Yoonsik-Shin <sys25800994@gmail.com>
2023-02-15 15:22:38 +0900

    수정

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,14 +1,14 @@
 import axios from "axios";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import { useDispatch, useSelector } from "react-redux";
-import '../login.css'
+import "../login.css";
 import { onLogin, onLogout, onSilentRefresh } from "../modules/token";
 import { loginUser, clearUser } from "../reducer/useSlice";
 import { useNavigate } from "react-router-dom";
 
 const Login = ({ login }) => {
-    const [cookies, setCookie, removeCookie] = useCookies(['id'])
+    const [cookies, setCookie, removeCookie] = useCookies(["id"]);
     const dispatch = useDispatch();
     const user = useSelector((state) => state.user);
     const navigate = useNavigate();
@@ -35,85 +35,77 @@ const Login = ({ login }) => {
         if (emailValid && pwValid) {
             setNotAllow(false);
             return;
-        } setNotAllow(true);
-
+        }
+        setNotAllow(true);
     }, [emailValid, pwValid]);
 
     useEffect(() => {
-        let testValue = document.cookie.split('; ').find((row) => row.startsWith('refresh'))?.split('=')[1];
-        if (testValue) onSilentRefresh(testValue)
-    }, [user])
+        let testValue = document.cookie
+            .split("; ")
+            .find((row) => row.startsWith("refresh"))
+            ?.split("=")[1];
+        if (testValue) onSilentRefresh(testValue);
+    }, [user]);
 
     // 이메일과 비밀번호가 모두 유효할 때(valid state가 변화할 때)
     const handleEmail = (e) => {
         setEmail(e.target.value);
         // 정규표현식으로 검사(문자열이 아니라 그냥 하네? vanila js에서는 test()함수 호출해서 문자열로 넣어줬었는데)
-        const regex =
-            /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+        const regex = /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
         if (regex.test(e.target.value)) {
             setEmailValid(true);
         } else {
             setEmailValid(false);
         }
-    }
+    };
 
     const handlePw = (e) => {
-        setPw(e.target.value)
-        const regex =
-            /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
+        setPw(e.target.value);
+        const regex = /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
         if (regex.test(e.target.value)) {
             setPwValid(true);
         } else {
             setPwValid(false);
         }
-    }
+    };
 
-    const onClickConfirmButton = (e) => {
+    const onClickConfirmButton = async (e) => {
         e.preventDefault();
 
         if (!email) {
-            return alert('아이디를 입력하세요.');
+            return alert("아이디를 입력하세요.");
         } else if (!pw) {
-            return alert('등록되지 않은 회원입니다.');
-        }
-
-        else {
+            return alert("등록되지 않은 회원입니다.");
+        } else {
             const data = {
                 email,
                 password: pw,
             };
-            onLogin(data);
-            dispatch(loginUser(data))
-            // navigate("/")
-
-
+            const result = await onLogin(data);
+            dispatch(loginUser({ email: result.email }));
+            // navigate("/");
         }
-    }
+    };
     // 실험을 위해 남겨놓기
     const onClickrefreshButton = (e) => {
         e.preventDefault();
         // console.log(testValue)
-        console.log("freshbutton")
-
-
-
+        console.log("freshbutton");
 
         // axios.post("http://127.0.0.1:8000/users/auth/refresh/")
         //     .then((res) => {
         //         axios.defaults.headers.common['Authorization'] = `Bearer ${res.data.access}`;
         //     }).catch(error => { });
-    }
+    };
 
     const onClickLogout = (e) => {
         e.preventDefault();
         onLogout();
         dispatch(clearUser());
-    }
+    };
 
     return (
-
         <div className="Login">
-
             <form>
                 <div className="titleWrap">
                     이메일과 비밀번호를
@@ -124,41 +116,18 @@ const Login = ({ login }) => {
                 <div className="contentWrap">
                     <div className="inputTitle">이메일 주소</div>
                     <div className="inputWrap">
-                        <input
-                            type={"text"}
-                            className="input"
-                            placeholder="test@gmail.com"
-                            value={email}
-                            onChange={handleEmail}
-                        />
+                        <input type={"text"} className="input" placeholder="test@gmail.com" value={email} onChange={handleEmail} />
                     </div>
                     {/* error msg */}
-                    <div className="errorMessageWrap">
-                        {
-                            !emailValid && email.length > 0 && (
-                                <div>올바른 이메일을 입력해주세요.</div>
-                            )
-                        }
-                    </div>
+                    <div className="errorMessageWrap">{!emailValid && email.length > 0 && <div>올바른 이메일을 입력해주세요.</div>}</div>
 
-                    <div className="inputTitle" style={{ marginTop: "26px" }}>비밀번호</div>
+                    <div className="inputTitle" style={{ marginTop: "26px" }}>
+                        비밀번호
+                    </div>
                     <div className="inputWrap">
-                        <input
-                            type={"password"}
-                            className="input"
-                            placeholder="영문, 숫자, 특수문자 포함 8자 이상"
-                            value={pw}
-                            onChange={handlePw}
-                        />
+                        <input type={"password"} className="input" placeholder="영문, 숫자, 특수문자 포함 8자 이상" value={pw} onChange={handlePw} />
                     </div>
-                    <div className="errorMessageWrap">
-                        {
-                            !pwValid && pw.length > 0 && (
-                                <div>영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.</div>
-                            )
-                        }
-                    </div>
-
+                    <div className="errorMessageWrap">{!pwValid && pw.length > 0 && <div>영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.</div>}</div>
                 </div>
                 <div>
                     <button type="submit" onClick={onClickConfirmButton} disabled={notAllow} className="bottomButton">
@@ -172,11 +141,8 @@ const Login = ({ login }) => {
                     </button>
                 </div>
             </form>
+        </div>
+    );
+};
 
-
-        </div >
-    )
-
-}
-
-export default Login
+export default Login;
--- src/navbar.js
+++ src/navbar.js
@@ -1,14 +1,14 @@
-import logo from './logo2.png'
-import { FaBars, FaTwitter } from 'react-icons/fa';
-import { links } from './data';
-import { useEffect, useLayoutEffect, useRef, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { asyncUpFetch, loginUser } from './reducer/useSlice';
-import { useCookies } from 'react-cookie';
-import { IoRefreshCircle } from 'react-icons/io5';
-import Profile from './component/Profile';
+import logo from "./logo2.png";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links } from "./data";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { asyncUpFetch, loginUser } from "./reducer/useSlice";
+import { useCookies } from "react-cookie";
+import { IoRefreshCircle } from "react-icons/io5";
+import Profile from "./component/Profile";
 
 export const Navbar = ({ re }) => {
     const dispatch = useDispatch();
@@ -17,29 +17,32 @@ export const Navbar = ({ re }) => {
     const linksContainerRef = useRef(null);
     const linksRef = useRef(null);
 
-    const status = useSelector(state => {
+    const status = useSelector((state) => {
         return state.user.status;
-    })
+    });
 
-    const refresh_token = document.cookie.split('; ').find((row) => row.startsWith('refresh'))?.split('=')[1]
+    const refresh_token = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("refresh"))
+        ?.split("=")[1];
 
-    const userInfo = useSelector(state => {
-        return state.user
-    })
+    const userInfo = useSelector((state) => {
+        return state.user;
+    });
 
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const linksHeight = linksRef.current.getBoundingClientRect().height;
-        // const linksHeight = linksRef.current.offsetHeight; 
+        // const linksHeight = linksRef.current.offsetHeight;
         // 거의 비슷하다. 데스크탑일 때: 24; 모바일일 때: 120;
 
         if (showLinks) {
-            linksContainerRef.current.style.height = `${linksHeight}px`
+            linksContainerRef.current.style.height = `${linksHeight}px`;
         } else {
-            linksContainerRef.current.style.height = '0px'
+            linksContainerRef.current.style.height = "0px";
         }
-    }, [showLinks])
+    }, [showLinks]);
 
     // useEffect(() => {
     //     axios.get('http://127.0.0.1:8000/users/auth/')
@@ -47,41 +50,47 @@ export const Navbar = ({ re }) => {
     // }, [])
 
     useEffect(() => {
-        console.log("데이터 fetch 시작")
+        console.log("데이터 fetch 시작");
         dispatch(asyncUpFetch());
-        console.log(status)
-    }, [])
+        console.log(status);
+    }, []);
 
     return (
         <nav>
             <div className="nav-center">
                 <div className="nav-header">
-                    <Link to="/"><img src={logo} className='logo' alt="logo" /></Link>
-                    <button className='nav-toggle' onClick={() => {
-                        setShowLinks(!showLinks)
-                    }}>
+                    <Link to="/">
+                        <img src={logo} className="logo" alt="logo" />
+                    </Link>
+                    <button
+                        className="nav-toggle"
+                        onClick={() => {
+                            setShowLinks(!showLinks);
+                        }}
+                    >
                         <FaBars />
                     </button>
                 </div>
-                <div className='links-container' ref={linksContainerRef}>
-                    <ul className='links' ref={linksRef}>
+                <div className="links-container" ref={linksContainerRef}>
+                    <ul className="links" ref={linksRef}>
                         {links.map((link) => {
                             const { id, url, text } = link;
-                            return <li key={id}>
-                                <a href={url}>{text}</a>
-                            </li>
+                            return (
+                                <li key={id}>
+                                    <a href={url}>{text}</a>
+                                </li>
+                            );
                         })}
                     </ul>
                 </div>
                 {/* 로딩 중 아직 이메일이 상태에 업데이트 안됐을 경우 */}
-                {status === 'loading' && !userInfo.email ? <div></div> : <div>{userInfo.email && userInfo.email}</div>}
+                {/* {status === "loading" && !userInfo.email ? <div></div> : <div>{userInfo.email && userInfo.email}</div>} */}
                 {/* 새로고침없이 로그아웃했을 경우 */}
-                {status === 'complete' && !userInfo.email && <Link to='login'>로그인</Link>}
+                {/* {status === "complete" && !userInfo.email && <Link to="login">로그인</Link>} */}
                 {/* 로그아웃한 상태에서 새로고침했을 경우(페이지 첫 진입) */}
-                {status === 'rejected' && !userInfo.email && <Link to='login'>로그인</Link>}
-
-
+                {/* {status === "rejected" && !userInfo.email && <Link to="login">로그인</Link>} */}
+                {userInfo.email ? <Link to="login">{userInfo.email}</Link> : <Link to="login">로그인</Link>}
             </div>
-        </nav >
-    )
-}
+        </nav>
+    );
+};
