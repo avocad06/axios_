a44743d7ab11ae3ad769c23ba1c39a39a641f3f2
avocad06 <miggo2704@gmail.com>
2023-02-15 17:04:58 +0900

    feat: 로그인 서버통신

--- src/modules/token.js
+++ src/modules/token.js
@@ -1,20 +1,31 @@
 import axios from "axios";
 import { getCookie, setCookie, removeCookie } from "./Cookies";
-export const onLogin = (body) => {
-
+export const onLogin = async (body) => {
+    /////////// 이부분 수정했습니다~~~~~~~~~~~~~~~~
+    /////////// 이부분 수정했습니다~~~~~~~~~~~~~~~~
+    /////////// 이부분 수정했습니다~~~~~~~~~~~~~~~~
+    const { email, password } = body;
     const data = {
-        email: body.email,
-        password: body.password,
+        email,
+        password,
     };
 
-    axios.post('http://127.0.0.1:8000/users/auth/', data, { withCredentials: true })
+    /////////// 이부분 수정했습니다~~~~~~~~~~~~~~~~
+    /////////// 이부분 수정했습니다~~~~~~~~~~~~~~~~
+    /////////// 이부분 수정했습니다~~~~~~~~~~~~~~~~
+    const result = await axios
+        .post("http://127.0.0.1:8000/users/auth/", data, { withCredentials: true })
         .then((res) => {
-
-            console.log(res)
-            setCookie('refresh', res.data.token.refresh)
-            return res.data.token.refresh
-        }).catch(error => { });
-}
+            setCookie("refresh", res.data.token.refresh);
+            return {
+                email: res.data.user.email,
+            };
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+    return result;
+};
 
 export const onSilentRefresh = (refresh) => {
     // axios.interceptors.response.use(res => {
@@ -25,34 +36,36 @@ export const onSilentRefresh = (refresh) => {
     //
     //   });
     if (refresh) {
-        console.log(refresh, 1)
-        axios.post('http://127.0.0.1:8000/users/auth/refresh/', { "refresh": String(refresh) })
+        console.log(refresh, 1);
+        axios
+            .post("http://127.0.0.1:8000/users/auth/refresh/", { refresh: String(refresh) })
             .then((response) => {
                 onAuthorize(response);
-                return response.data
+                return response.data;
             })
-            .catch(error => { });
+            .catch((error) => {});
     }
-
-}
+};
 
 // 인가함수
-export const onAuthorize = response => {
+export const onAuthorize = (response) => {
+    let testValue = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("refresh"))
+        ?.split("=")[1];
 
-    let testValue = document.cookie.split('; ').find((row) => row.startsWith('refresh'))?.split('=')[1];
-
-    axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.access}`;
+    axios.defaults.headers.common["Authorization"] = `Bearer ${response.data.access}`;
     console.log(axios.defaults.headers);
-    setTimeout(() => { onSilentRefresh(testValue) }, 1000 * 60 * 4);
-
-}
+    setTimeout(() => {
+        onSilentRefresh(testValue);
+    }, 1000 * 60 * 4);
+};
 
 export const onLogout = () => {
-    axios.delete("http://127.0.0.1:8000/users/auth/")
-        .then((res) => {
-            console.log(res)
-            removeCookie('refresh');
+    axios.delete("http://127.0.0.1:8000/users/auth/").then((res) => {
+        console.log(res);
+        removeCookie("refresh");
 
-            axios.defaults.headers.common['Authorization'] = null;
-        })
-}
+        axios.defaults.headers.common["Authorization"] = null;
+    });
+};
